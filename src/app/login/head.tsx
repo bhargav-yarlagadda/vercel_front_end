e09b471c85333ce046c2d9139c228a7eb16f054a
bhargav-yarlagadda <bhargavyarlagadda2003@gmail.com>
2025-09-14 12:38:42 +0530

    chore.build fixes

---
+++ src/app/login/head.tsx
@@ -0,0 +1,3 @@
+export default function Head() {
+  return <title>Lume | Login</title>;
+}
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
-import dynamic from 'next/dynamic';
-import { Metadata } from 'next';
-const Login = dynamic(() => import('@/pages/LoginPage'));
-export const metadata:Metadata={
-  title:"Lume | Login"
-}
+"use client";
+import React from "react";
+import dynamic from "next/dynamic";
+
+const Login = dynamic(() => import("@/ui_pages/LoginPage"), { ssr: false });
+
 const Page = () => {
   return (
     <div>
